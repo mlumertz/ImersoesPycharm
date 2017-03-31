e47d4cb73aafaeae417b5b3c63f2f0df04568164
marcio <mlumertz@gmail.com>
2017-03-31 11:58:51 +0900

    jquery

--- Feedbacks/static/js/jquery.formset.js
+++ Feedbacks/static/js/jquery.formset.js
@@ -218,7 +218,7 @@
         prefix: 'form',                  // The form prefix for your django formset
         formTemplate: null,              // The jQuery selection cloned to generate new form instances
         addText: 'Adicionar novo contato',          // Text for the add link
-        deleteText: 'Remover',            // Text for the delete link
+        deleteText: '<span class="glyphicon glyphicon-trash"></span>',            // Text for the delete link
         addCssClass: 'add-row',          // CSS class applied to the add link
         deleteCssClass: 'delete-row',    // CSS class applied to the delete link
         formCssClass: 'dynamic-form',    // CSS class applied to each form in a formset
